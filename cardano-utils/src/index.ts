4ac71199e355f974effa413a79f09b4ceacbf84e
rjlacanlaled <rjlacanlaled@gmail.com>
2022-08-22 16:39:14 +0800

    setup package

---
+++ cardano-utils/src/index.ts
@@ -0,0 +1,15 @@
+import * as Wallet from './utils/wallet-utils';
+import * as Epoch from './utils/epoch-utils';
+import * as Faucet from './utils/faucet-utils';
+import * as String from './utils/string-utils';
+import * as Transaction from './utils/tx-utils';
+import * as Utxo from './utils/utxo-utils';
+
+export default {
+    Wallet,
+    Epoch,
+    Faucet,
+    String,
+    Transaction,
+    Utxo,
+};
